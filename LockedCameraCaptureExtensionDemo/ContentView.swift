77216eb380e67a66948af0d90477fdd77d29e3b9
juniperphoton <dengweichao@hotmail.com>
2024-08-20 19:20:10 +0800

    Initial Commit

---
+++ LockedCameraCaptureExtensionDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockedCameraCaptureExtensionDemo
+//
+//  Created by Photon Juniper on 2024/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LockedCameraCaptureExtensionDemo/LockedCameraCaptureExtensionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockedCameraCaptureExtensionDemoApp.swift
+//  LockedCameraCaptureExtensionDemo
+//
+//  Created by Photon Juniper on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct LockedCameraCaptureExtensionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
